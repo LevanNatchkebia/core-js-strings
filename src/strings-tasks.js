dd6687beeebd1b5fb6153f0bf8edcbf71afa1550
Levan Natchkebia <levan.natchkebia@hotmail.com>
2024-01-05 15:26:48 +0400

    fix: fix warning

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -494,7 +494,7 @@ function extractEmails(str) {
  *
  */
 function encodeToRot13(str) {
-  return str.replace(/[a-zA-Z]/g, function (char) {
+  return str.replace(/[a-zA-Z]/g, (char) => {
     const isUpperCase = char === char.toUpperCase();
     const offset = isUpperCase ? 65 : 97;
     return String.fromCharCode(
